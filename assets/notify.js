113346162380061a00501517721a4c5bca36bd90
pinktran <hongtran@vmax.vn>
2022-03-20 12:35:43 +0700

    up file new

--- assets/notify.js
+++ assets/notify.js
@@ -159,7 +159,7 @@
         
         var name = form.find('input[name="name"]');
         var phone = form.find('input[name="phone"]');
-        var url = 'https://api.rentracksw.com/lead/create?site_id=8936&advertiser_id=181&product_id=70696&token=qhy1b6LMDE5YmM1NjY3N2ZlOGM1YjA2NzBiNzY5ZDNhMjNm' + '&name=' + `${name.val()}` + '&phone=' + `${phone.val()}`;
+        var url = 'https://api.rentracksw.com/lead/create?site_id=9246&advertiser_id=181&product_id=70696&token=rq0Vo7JZWRlNzY4ZTQ2YzhhNTUyMDA2ZGRjMzgxMDBjZDNi' + '&name=' + `${name.val()}` + '&phone=' + `${phone.val()}`;
         const toSend = {
             name: `${name.val()}`,
             phone: `${phone.val()}`
